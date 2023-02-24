ddfb9b8e4fc1cc325b0fd5b6fde42c0987747f43
Marco Collovati <mcollovati@gmail.com>
2023-02-24 11:57:35 +0100

    chore: upgrade to Vaadin 24

--- spinkit-demo/src/main/java/org/vaadin/spinkit/demo/DemoUI.java
+++ spinkit-demo/src/main/java/org/vaadin/spinkit/demo/DemoUI.java
@@ -15,16 +15,6 @@
  */
 package org.vaadin.spinkit.demo;
 
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import com.github.appreciated.css.grid.sizes.Length;
-import com.github.appreciated.css.grid.sizes.Repeat;
-import com.github.appreciated.layout.FlexibleGridLayout;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -33,6 +23,7 @@ import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
@@ -45,6 +36,13 @@ import org.vaadin.spinkit.Spinner;
 import org.vaadin.spinkit.SpinnerSize;
 import org.vaadin.spinkit.SpinnerType;
 
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 @PageTitle("Vaadin Spinkit Add-on Demo")
 @Route("")
@@ -53,30 +51,29 @@ public class DemoUI extends Div {
 
     public DemoUI() {
         VTabSheet tabSheet = new VTabSheet();
-        tabSheet.setFlexGrowForEnclosedTabs(1);
         tabSheet.addTab("Spinners", spinnersContainer());
         tabSheet.addTab("Sizes", spinnerSizesContainer());
         tabSheet.addTab("Source code", new RichText().withMarkDown(getClass().getResourceAsStream("source.md")));
         tabSheet.addTab("Dialog", new Button("Open dialog", ev -> openDialog()));
 
         RichText info = new RichText()
-            .withMarkDown(getClass().getResourceAsStream("about.md"));
+                .withMarkDown(getClass().getResourceAsStream("about.md"));
 
 
         VVerticalLayout layout = new VVerticalLayout()
-            .withMargin(true)
-            .withComponent(info).addExpanded(tabSheet)
-            .withFullHeight().withFullWidth();
+                .withMargin(true)
+                .withComponent(info).addExpanded(tabSheet)
+                .withFullHeight().withFullWidth();
 
         add(layout);
     }
 
     private void openDialog() {
         Dialog dialog = new Dialog(
-            new VVerticalLayout(createSpinner(SpinnerType.CIRCLE)
-                .extraLarge().withDisplayBlock())
-                .withJustifyContentMode(FlexComponent.JustifyContentMode.CENTER)
-                .withSize("300px", "300px"));
+                new VVerticalLayout(createSpinner(SpinnerType.CIRCLE)
+                        .extraLarge().withDisplayBlock())
+                        .withJustifyContentMode(FlexComponent.JustifyContentMode.CENTER)
+                        .withSize("300px", "300px"));
         dialog.setCloseOnEsc(true);
         dialog.setCloseOnOutsideClick(true);
         dialog.open();
@@ -84,20 +81,15 @@ public class DemoUI extends Div {
 
     private Component spinnersContainer() {
         List<Spinner> spinners = Stream.of(SpinnerType.values()).filter(t -> !t.isAlias())
-            .map(DemoUI::createSpinner)
-            .collect(Collectors.toList());
+                .map(DemoUI::createSpinner)
+                .collect(Collectors.toList());
 
-        FlexibleGridLayout spinnersContainer = new FlexibleGridLayout()
-            .withColumns(Repeat.RepeatMode.AUTO_FILL, new Length("25%"))
-            .withPadding(true)
-            .withSpacing(true)
-            .withItems(spinners.stream().map(s -> spinnerWithName(s, Spinner::getType)).toArray(Component[]::new));
+        FlexLayout spinnersContainer = toFlexLayout(spinners);
 
         TextField color = new TextField("Color (--sk-color)", "#333");
         color.addValueChangeListener(e -> spinners.forEach(s -> s.setColor(e.getValue())));
 
         ComboBox<String> theme = new ComboBox<>("Css class", "", "green", "red");
-        theme.setPreventInvalidInput(true);
         theme.addValueChangeListener(e -> spinners.forEach(s -> {
             Optional.ofNullable(e.getOldValue()).ifPresent(css -> s.removeClassName("sk-demo-" + css));
             s.addClassName("sk-demo-" + e.getValue());
@@ -120,29 +112,24 @@ public class DemoUI extends Div {
     private Component spinnerSizesContainer() {
 
         List<Spinner> spinners = EnumSet.complementOf(EnumSet.of(SpinnerSize.DEFAULT)).stream()
-            .map(size -> {
-                Spinner s = createSpinner(SpinnerType.PLANE);
-                s.setSize(size);
-                s.setTitle(size.name());
-                return s;
-            }).collect(Collectors.toList());
+                .map(size -> {
+                    Spinner s = createSpinner(SpinnerType.PLANE);
+                    s.setSize(size);
+                    s.setTitle(size.name());
+                    return s;
+                }).collect(Collectors.toList());
 
         List<SpinnerType> spinnerTypes = Stream.of(SpinnerType.values())
-            .filter(t -> !t.isAlias()).collect(Collectors.toList());
+                .filter(t -> !t.isAlias()).collect(Collectors.toList());
 
         ComboBox<SpinnerType> selector = new ComboBox<>("Select spinner type", spinnerTypes);
-        selector.setPreventInvalidInput(true);
         selector.setValue(SpinnerType.ROTATING_PLANE);
         selector.addValueChangeListener(e -> spinners.forEach(s -> s.setType(selector.getValue())));
 
         TextField baseSize = new TextField("Base size (--sk-size)", "40px");
         baseSize.addValueChangeListener(e -> spinners.forEach(s -> s.setBaseSize(e.getValue())));
 
-        FlexibleGridLayout spinnersContainer = new FlexibleGridLayout()
-            .withColumns(Repeat.RepeatMode.AUTO_FILL, new Length("25%"))
-            .withPadding(true)
-            .withSpacing(true)
-            .withItems(spinners.stream().map(s -> spinnerWithName(s, Spinner::getSize)).toArray(Component[]::new));
+        FlexLayout spinnersContainer = toFlexLayout(spinners);
 
         for (SpinnerSize size : EnumSet.complementOf(EnumSet.of(SpinnerSize.DEFAULT))) {
             Spinner spinner = new Spinner(SpinnerType.PLANE);
@@ -167,6 +154,19 @@ public class DemoUI extends Div {
     }
 
 
+    private static FlexLayout toFlexLayout(List<Spinner> spinners) {
+        Component[] elements = spinners.stream()
+                .map(s -> spinnerWithName(s, Spinner::getType))
+                .toArray(Component[]::new);
+        FlexLayout spinnersContainer = new FlexLayout(elements);
+        spinnersContainer.setFlexGrow(0, elements);
+        spinnersContainer.setFlexBasis("25%", elements);
+        spinnersContainer.setFlexDirection(FlexLayout.FlexDirection.ROW);
+        spinnersContainer.setFlexWrap(FlexLayout.FlexWrap.WRAP);
+        return spinnersContainer;
+    }
+
+
     private static Spinner createSpinner(SpinnerType t) {
         Spinner spinner = new Spinner(t);
         spinner.setTitle(t.name());
@@ -176,6 +176,6 @@ public class DemoUI extends Div {
 
     private static VVerticalLayout spinnerWithName(Spinner s, Function<Spinner, Enum<?>> textFn) {
         return new VVerticalLayout().withComponent(s)
-            .withComponent(new Span(textFn.apply(s).toString()), FlexComponent.Alignment.CENTER);
+                .withComponent(new Span(textFn.apply(s).toString()), FlexComponent.Alignment.CENTER);
     }
 }
